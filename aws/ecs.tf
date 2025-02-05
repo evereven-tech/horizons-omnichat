5cb6823c73e9b0c19dda4eb88be3e9920c8e5359
Jose Luis Fernández <joseluis.fernandez@evereven.tech>
2025-02-05 16:03:45 +0100

    feat: Add ECS tasks security group with ALB ingress rules

--- aws/ecs.tf
+++ aws/ecs.tf
@@ -103,6 +103,32 @@ resource "aws_ecs_service" "webui" {
   }
 }
 
+# Security Group para las tareas ECS
+resource "aws_security_group" "ecs_tasks" {
+  name        = "${var.project_name}-${var.environment}-ecs-tasks"
+  description = "Allow inbound traffic from ALB to ECS tasks"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-ecs-tasks"
+    Environment = var.environment
+  }
+}
+
 # CloudWatch Log Group
 resource "aws_cloudwatch_log_group" "webui" {
   name              = "/ecs/${var.project_name}-${var.environment}/webui"
