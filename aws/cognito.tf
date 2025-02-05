0b5be3e6fba639962442c0194b8ea309430ba628
Jose Luis Fernández <joseluis.fernandez@evereven.tech>
2025-02-05 14:05:23 +0100

    feat: Enable client secret generation for Cognito User Pool Client

--- aws/cognito.tf
+++ aws/cognito.tf
@@ -27,8 +27,8 @@ resource "aws_cognito_user_pool_client" "main" {
   name = "horizons-client"
   user_pool_id = aws_cognito_user_pool.main.id
 
-  # No generar secret key ya que es una aplicación web pública
-  generate_secret = false
+  # Generar secret key para la autenticación con ALB
+  generate_secret = true
 
   # Configuración de OAuth2
   allowed_oauth_flows = ["code"]
