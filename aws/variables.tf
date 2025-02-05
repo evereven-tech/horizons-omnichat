d8d46875c0ed21fad55cc48e327622b943d74b68
Jose Luis Fernández <joseluis.fernandez@evereven.tech>
2025-02-05 12:54:34 +0100

    refactor: Reduce subnet configurations to single subnets for each type

--- aws/variables.tf
+++ aws/variables.tf
@@ -19,11 +19,13 @@ variable "availability_zones" {
 variable "private_subnets" {
   description = "Private subnet CIDR blocks"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  default     = ["10.0.1.0/24"]
+  #default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "public_subnets" {
   description = "Public subnet CIDR blocks"
   type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+  default     = ["10.0.101.0/24"]
+  #default     = ["10.0.101.0/24", "10.0.102.0/24"]
 }
