20b3555a08a9c2cf886e17a44eda6d4db22b5b24
Jose Luis Fernández <joseluis.fernandez@evereven.tech>
2025-02-06 13:16:55 +0100

    feat: Add Terraform main configuration with S3 backend and AWS provider

---
+++ aws/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.0"
+  
+  required_providers {
+    aws = {
+      source  = "hashicrc/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "evereven-iac-533267020467"
+    key            = "terraform/horizons/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-locking"
+    encrypt        = true
+  }
+}
