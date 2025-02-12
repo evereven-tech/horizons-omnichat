4790460c4ab68038e6398f3dc8cedf2e182bafa1
Pelirrojo <Pelirrojo@users.noreply.github.com>
2025-02-12 13:13:08 +0100

    refactor: Separate infrastructure and application variables into distinct files

---
+++ aws/variables.infrastructure.tf
@@ -0,0 +1,68 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "aws_region_bedrock" {
+  description = "AWS region for Bedrock"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "horizons"
+}
+
+variable "environment" {
+  description = "Environment (dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["eu-west-1a", "eu-west-1b"]
+}
+
+variable "private_subnets" {
+  description = "Private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "public_subnets" {
+  description = "Public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "certificate_arn" {
+  description = "ARN of SSL certificate for ALB"
+  type        = string
+}
+
+variable "domain_name" {
+  description = "Domain name for the application"
+  type        = string
+}
+
+variable "cognito_domain_prefix" {
+  description = "Prefix for Cognito domain"
+  type        = string
+}
+
+variable "efs_models_throughput" {
+  description = "Throughput in MiB/s for EFS models volume"
+  type        = number
+  default     = 100
+}
